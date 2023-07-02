6fc8d1a7d70305af8c3a79b15a8d48c6430e6acb
MohammadArzouni <93600244+MohammadArzouni@users.noreply.github.com>
2023-07-02 17:15:01 +0300

    Creation of JS and the beginning code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+$(document).ready(function() {
+  // Array to store the TV shows inputted by the user
+  var tvShows = [];
 
+  // Function to add the TV show to the list
+  function addTVShow(name, genre) {
+    tvShows.push({ name: name, genre: genre });
+    createTableRow(name, genre);
+  }
